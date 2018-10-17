d6c995318455c49cce805ae00253dc0cdff2ac3c
MatthieuJehanno <matthieujehanno@outlook.fr>
2018-10-17 22:54:08 +0200

    s

---
+++ application/controllers/Inscript.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Inscript extends CI_Controller {
+    public function index()
+    {
+        $this->load->view('Inscription');
+    }
+
+    public function Inscription(){
+         $this->form_validation_set_rules
+    }
+}
+
+
+?>
---
+++ application/views/Inscription.php
@@ -0,0 +1,30 @@
+<<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page d'inscription</title>
+   
+</head>
+<body>
+
+            <h2><center>Inscrivez-vous</center> </h2> 
+         
+                <h5>Nom</h5>
+                <input type="text" class="form-control" placeholder="Nom+prénom" name="nomUser"  value="Votre nom">
+            
+            
+                <h5>Identifiant</h5>
+                <input type="text" class="form-control" placeholder="login" name="login"  value="Votre identifiant">
+            
+                <h5>Mot de passe</h5>
+                <input type="password" class="form-control" placeholder="Mot de passe" name="mdp" value="Votre mot de passe">
+            
+
+           
+                 <button  type="submit" value="Inscription" class="btn btn-primary" name="register" </button>
+    
+
+
+</body>
+</html>
